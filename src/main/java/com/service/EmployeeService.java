bf46409df458781bbaa59a48e92db1151a800020
bhishma neupane <bhishmaneupane@bhishmas-MBP.lan>
2024-05-27 18:36:30 -0500

    new branch

---
+++ src/main/java/com/service/EmployeeService.java
@@ -0,0 +1,21 @@
+package com.service;
+
+import java.util.List;
+
+import com.dto.EmployeeDTO;
+
+public interface EmployeeService {
+
+	void registerEmp(EmployeeDTO employeeDTO);
+
+	EmployeeDTO authenticate(String emailId, String password);
+
+	List<EmployeeDTO> findAllEmployees();
+
+	void deleteEmp(int employeeId);
+
+	EmployeeDTO retrieveRecord(int employeeId);
+
+	void updateKaro(EmployeeDTO employeeDTO);
+
+}
